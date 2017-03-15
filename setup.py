0fd04eecb0b70597f569fb7c8ccf3be6d21279bb
Momme Butenschön <momm@pml.ac.uk>
2017-03-15 13:48:23 +0000

    Fixed name of package dependency.

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib','mpl_toolkits.basemap','numpy','scipy','netCDF4'],
+    install_requires=['matplotlib','basemap','numpy','scipy','netCDF4'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
