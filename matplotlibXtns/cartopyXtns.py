eb18c009cd7de1152ca85174efef569e23002f77
Momme Butenschön <momme.butenschon@cmcc.it>
2019-12-18 18:03:43 +0100

    Added regional maps. Currently extrapolates into empty space, needs sorting of mask.

--- matplotlibXtns/cartopyXtns.py
+++ matplotlibXtns/cartopyXtns.py
@@ -16,8 +16,9 @@ if cartopy_installed:
     from pdb import set_trace
     from matplotlib.pyplot import figure
     from numpy.ma import getmaskarray,masked_where
-    class globalOceanMap:
-        def __init__(self,lon0=0.,prj=crs.Mollweide,*args,**opts):
+
+    class oceanMap:
+        def __init__(self,lon0=0.,prj=crs.PlateCarree,*args,**opts):
             """Class for global maps using cartopy Mollweide or PlateCarree projections.
 
             Args:
@@ -39,35 +40,47 @@ if cartopy_installed:
             self.ref_prj=crs.PlateCarree()
             #get bounding box of the globe in native coordinates
 
-        def contourf(self,*args,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
+        def contourf(self,x,y,*args,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
             if not f:
                 f=figure(figsize=[12,6])
             if not ax:
                 ax=f.add_subplot(111,projection=self.prj)
-            cnt=ax.contourf(*args,**opts,zorder=-1)
+            cnt=ax.contourf(x,y,*args,**opts,zorder=-1)
             ax.coastlines(land_res)
             if land_colour:
                 print("Filling continents")
                 ax.add_feature(feature.LAND, facecolor=land_colour)
-            if colourbar: cb=f.colorbar(cnt,ax=ax,shrink=.5,aspect=10)
+            #ax.set_extent([xmin,xmax,ymin,ymax])
+            if colourbar: cb=f.colorbar(pcm,ax=ax,shrink=.5,aspect=10)
             return cnt,cb
 
-        def pcolormesh(self,*args,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
+        def pcolormesh(self,x,y,*args,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
             if not f:
                 f=figure(figsize=[12,6])
             if not ax:
                 ax=f.add_subplot(111,projection=self.prj)
-            cnt=ax.pcolormesh(*args,**opts,zorder=-1)
+            pcm=ax.pcolormesh(x,y,*args,**opts,zorder=-1)
             ax.coastlines(land_res)
             if land_colour:
                 print("Filling continents")
                 ax.add_feature(feature.LAND, facecolor=land_colour)
-            if colourbar: cb=f.colorbar(cnt,ax=ax,shrink=.5,aspect=10)
-            return cnt,cb
+            if colourbar: cb=f.colorbar(pcm,ax=ax,shrink=.5,aspect=10)
+            return pcm,cb
 
-        def interpolate(self,lon,lat,data,*args,res=360.,bounds=False,**opts):
-            xmin,xmax=self.prj.x_limits
-            ymin,ymax=self.prj.y_limits
+        def interpolate(self,lon,lat,data,*args,res=360.,bounds=False,zoom=0,**opts):
+            if zoom:
+                xyminmax=self.prj.transform_points(self.ref_prj,lon.ravel(),lat.ravel())
+                xmin,xmax=xyminmax[:,0].min(),xyminmax[:,0].max()
+                ymin,ymax=xyminmax[:,1].min(),xyminmax[:,1].max()
+                Dx=xmin-xmax
+                Dy=ymin-ymax
+                xmin=xmin-(zoom-100.)/200.*Dx
+                xmax=xmax+(zoom-100.)/200.*Dx
+                ymax=ymax+(zoom-100.)/200.*Dy
+                ymin=ymin-(zoom-100.)/200.*Dy
+            else:
+                xmin,xmax=self.prj.x_limits
+                ymin,ymax=self.prj.y_limits
             dx=(xmax-xmin)/res
             dy=2.*(ymax-ymin)/res
             #regular target coordinates:
@@ -104,13 +117,58 @@ if cartopy_installed:
             else:
                 return x,y,dxy.reshape(xx.shape)
 
-        def interpolated_contourf(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
-            x,y,d=self.interpolate(lon,lat,data,res=res)
+        def interpolated_contourf(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,zoom=0,**opts):
+            x,y,d=self.interpolate(lon,lat,data,res=res,zoom=zoom)
+            print("interpolated coordinate range:",xb.min(),xb.max(),yb.min(),yb.max())
+            print("map coordinate range:",self.prj.x_limits,self.prj.y_limits)
             return self.contourf(x,y,d,*args,land_colour=land_colour,f=f,ax=ax,colourbar=colourbar,**opts)
 
-        def interpolated_pcolormesh(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,**opts):
-            x,y,d,xb,yb=self.interpolate(lon,lat,data,res=res,bounds=True)
+        def interpolated_pcolormesh(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,zoom=0,**opts):
+            x,y,d,xb,yb=self.interpolate(lon,lat,data,res=res,bounds=True,zoom=zoom)
+            print("interpolated coordinate range:",xb.min(),xb.max(),yb.min(),yb.max())
+            print("map coordinate range:",self.prj.x_limits,self.prj.y_limits)
             return self.pcolormesh(xb,yb,d,*args,land_colour=land_colour,f=f,ax=ax,colourbar=colourbar,**opts)
+
+    class globalOceanMap(oceanMap):
+        def __init__(self,lon0=0.,prj=crs.Mollweide,*args,**opts):
+            oceanMap.__init__(self,lon0=lon0,prj=prj,*args,**opts)
+
+    class regionalOceanMap(oceanMap):
+        def __init__(self,lon0=0.,lat0=0.,prj=crs.AlbersEqualArea,*args,**opts):
+            if type(prj)==str:
+                if prj.lower().strip()=="albersequalarea":
+                    prj=crs.AlbersEqualArea
+                elif prj.lower().strip()=="platecarree":
+                    prj=crs.PlateCarree
+                else:
+                    print("{} projection not implemented, using AlbersEqualArea instead...".format(prj))
+                    prj=crs.AlbersEqualArea
+            if array(lon0).ndim>0:
+                try:
+                    lon0=(lon0.max()-lon0.min())/2.
+                except AttributeError:
+                    lon0=(max(lon0)-min(lon0))
+            if array(lat0).ndim>0:
+                try:
+                    lat0=(lat0.max()-lat0.min())/2.
+                except AttributeError:
+                    lat0=(max(lat0)-min(lat0))
+            if prj==crs.AlbersEqualArea:
+                print("Initialising regional AlbersEqualArea projection centred at {}N,{}E".format(lat0,lon0))
+                self.prj=prj(central_longitude=lon0,central_latitude=lat0,*args,**opts)
+            else:
+                print("Initialising regional PlateCarree projection centred at {}E".format(lon0))
+                self.prj=prj(lon0,*args,**opts)
+            self.ref_prj=crs.PlateCarree()
+
+        def interpolated_contourf(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,zoom=101,**opts):
+            return oceanMap.interpolated_contourf(self,lon,lat,data,*args,res=res,land_colour=land_colour,land_res=land_res,f=f,ax=ax,colourbar=colourbar,zoom=zoom,**opts)
+
+        def interpolated_pcolormesh(self,lon,lat,data,*args,res=360.,land_colour="#485259",land_res='50m',f=False,ax=False,colourbar=True,zoom=101,**opts):
+            return oceanMap.interpolated_pcolormesh(self,lon,lat,data,*args,res=res,land_colour=land_colour,land_res=land_res,f=f,ax=ax,colourbar=colourbar,zoom=zoom,**opts)
+
+        def interpolate(self,lon,lat,data,*args,res=360.,bounds=False,zoom=101,**opts):
+            return oceanMap.interpolate(self,lon,lat,data,*args,res=res,bounds=bounds,zoom=zoom,**opts)
 
     def mask_feature(x2d,y2d,feat=feature.LAND,eps=1.e-5):
             Mask=ones(x2d.shape,bool)
