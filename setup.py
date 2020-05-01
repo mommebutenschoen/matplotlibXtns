e1e860feabb9a0c298cf3b0d96deaa202b7ebdf2
Momme Butenschön <momme.butenschon@cmcc.it>
2020-05-01 12:25:28 +0200

    Optional dependencies.

--- setup.py
+++ setup.py
@@ -88,8 +88,8 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': [],
+        'geographic distances': ['pyproj',],
+        'geographic mapping': ['cartopy',],
     },
 
     # If there are data files included in your packages that need to be
